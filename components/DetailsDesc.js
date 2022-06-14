2f8b40fd47402983f9c8a9e36b9b9b407859f1d2
Simón Bustamante Alzate <simonba97@hotmail.com>
2022-06-14 14:08:48 -0500

    first commit

---
+++ components/DetailsDesc.js
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { View, Text } from 'react-native';
+import { EthPrice, NFTTitle } from './SubInfo';
+
+
+import { COLORS, SISEZ, FONTS, SIZES } from '../constants';
+
+const DetailsDesc = ({ data }) => {
+
+    const [textDescription, setTextDescription] = useState(data.description.slice(0, 100));
+    const [readMore, setReadMore] = useState(false);
+    return (
+        <>
+            <View style={{
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}>
+                <NFTTitle
+                    title={data.name}
+                    subTitle={data.creator}
+                    titleSize={SIZES.extraLarge}
+                    subTitleSize={SIZES.font}
+                />
+                <EthPrice price={data.price} />
+            </View>
+            <View style={{ marginVertical: SIZES.extraLarge * 1.5 }}>
+                <Text style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary
+                }}>Description</Text>
+                <View style={{ marginTop: SIZES.base }}>
+                    <Text style={{
+                        fontSize: SIZES.small,
+                        fontFamily: FONTS.regular,
+                        color: COLORS.secondary,
+                        lineHeight: SIZES.large
+                    }}>
+                        {textDescription}
+                        {!readMore && '...'}
+                        <Text
+                            style={{
+                                fontSize: SIZES.small,
+                                fontFamily: FONTS.semiBold,
+                                color: COLORS.primary
+                            }}
+                            onPress={() => {
+                                if (!readMore) {
+                                    setTextDescription(data.description);
+                                    setReadMore(true);
+                                } else {
+                                    setTextDescription(data.description.slice(0, 100));
+                                    setReadMore(false);
+                                }
+                            }}
+                        >
+                            {readMore ? ' Show less' : ' Show more'}
+                        </Text>
+                    </Text>
+
+                </View>
+            </View>
+        </>
+    )
+}
+
+export default DetailsDesc;
